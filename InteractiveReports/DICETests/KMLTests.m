412fa28dc0ba2c8184dc7b99d01246e710dc3857
stjohnr <stjohnr@bit-sys.com>
2014-12-18 14:26:09 -0700

    kml: progress commit

---
+++ InteractiveReports/DICETests/KMLTests.m
@@ -0,0 +1,43 @@
+//
+//  KMLTests.m
+//  InteractiveReports
+//
+//  Created by Robert St. John on 12/18/14.
+//  Copyright (c) 2014 mil.nga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+//#import "SimpleKML.h"
+
+@interface KMLTests : XCTestCase
+
+@end
+
+@implementation KMLTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+//    SimpleKML *kml;
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
