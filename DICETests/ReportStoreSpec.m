142ba79442921cd9e70c4453073a296a3fc4966f
restjohn <restjohn@users.noreply.github.com>
2015-06-02 20:12:13 -0600

    file_handling: updated ReportStore and tests to handle removing reports from the list whose files do not exist

--- DICETests/ReportStoreSpec.m
+++ DICETests/ReportStoreSpec.m
@@ -25,28 +25,78 @@ describe(@"ReportStore", ^{
     [[given([blueType couldHandleFile:nil]) withMatcher:endsWith(@".blue")] willReturnBool:YES];
     [[given([blueType couldHandleFile:nil]) withMatcher:isNot(endsWith(@".blue"))] willReturnBool:NO];
 
-    ReportStore *store = [[ReportStore alloc] init];
-    store.reportTypes = @[
-        redType,
-        blueType
-    ];
+    NSFileManager *fileManager = mock([NSFileManager class]);
+
+    NSString *reportsDirName = @"/dice/reports";
+    NSURL *reportsDirUrl = [NSURL fileURLWithPath:reportsDirName];
+
+    __block ReportStore *store;
 
     beforeAll(^{
 
     });
     
     beforeEach(^{
-
+        // initialize a new ReportStore to ensure all tests are independent
+        store = [[ReportStore alloc] initWithReportsDir:reportsDirUrl fileManager:fileManager];
+        store.reportTypes = @[
+            redType,
+            blueType
+        ];
     });
 
     describe(@"loadReports", ^{
 
-        it(@"finds the supported files in the reports directory", ^{
-            failure(@"unimplemented");
+        beforeEach(^{
+            [given([fileManager URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil]) willReturn:reportsDirUrl];
         });
 
-        it(@"removes reports for files no longer in the reports directory", ^{
-            failure(@"unimplemented");
+        it(@"finds the supported files in the reports directory", ^{
+            [given([fileManager contentsOfDirectoryAtURL:reportsDirUrl
+                includingPropertiesForKeys:nil
+                options:0
+                error:nil])
+                willReturn:@[
+                    [reportsDirUrl URLByAppendingPathComponent:@"report1.red"],
+                    [reportsDirUrl URLByAppendingPathComponent:@"report2.blue"],
+                    [reportsDirUrl URLByAppendingPathComponent:@"something.else"]
+                ]];
+
+            NSArray *reports = [store loadReports];
+
+            expect(reports.count).to.equal(2);
+            expect(((Report *)reports[0]).url).to.equal([reportsDirUrl URLByAppendingPathComponent:@"report1.red"]);
+            expect(((Report *)reports[1]).url).to.equal([reportsDirUrl URLByAppendingPathComponent:@"report2.blue"]);
+        });
+
+        it(@"removes enabled reports for files no longer in the reports directory", ^{
+            [[given([fileManager contentsOfDirectoryAtURL:reportsDirUrl
+                includingPropertiesForKeys:nil
+                options:0
+                error:nil])
+                willReturn:@[
+                    [reportsDirUrl URLByAppendingPathComponent:@"report1.red"],
+                    [reportsDirUrl URLByAppendingPathComponent:@"report2.blue"]
+                ]]
+                willReturn : @[
+                    [reportsDirUrl URLByAppendingPathComponent:@"report2.blue"]
+                ]];
+
+            NSArray *reports = [store loadReports];
+
+            expect(reports.count).to.equal(2);
+            expect(((Report *)reports[0]).url).to.equal([reportsDirUrl URLByAppendingPathComponent:@"report1.red"]);
+            expect(((Report *)reports[1]).url).to.equal([reportsDirUrl URLByAppendingPathComponent:@"report2.blue"]);
+
+            ((Report *)reports[0]).isEnabled = YES;
+            ((Report *)reports[1]).isEnabled = YES;
+
+            [given([fileManager fileExistsAtPath:((Report *)reports[0]).url.path]) willReturnBool:NO];
+
+            reports = [store loadReports];
+
+            expect(reports.count).to.equal(1);
+            expect(((Report *)reports[0]).url).to.equal([reportsDirUrl URLByAppendingPathComponent:@"report2.blue"]);
         });
 
     });
@@ -75,6 +125,11 @@ describe(@"ReportStore", ^{
             Report *report = [store attemptToImportReportFromResource:url];
 
             expect(store.reports).to.contain(report);
+            expect(report.reportID).to.equal(report.url.path);
+            expect(report.title).to.equal(report.url.lastPathComponent);
+            expect(report.summary).to.equal(@"Importing...");
+            expect(report.error).to.beNil;
+            expect(!report.isEnabled);
         });
 
     });
