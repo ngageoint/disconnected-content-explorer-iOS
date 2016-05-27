96b84c113d50e6a60c8b41b7e764cbd0132bd6e1
restjohn <restjohn@users.noreply.github.com>
2016-05-27 15:49:58 -0600

    removed OCMock from ParseJsonOperationSpec

--- DICETests/ParseJsonOperationSpec.m
+++ DICETests/ParseJsonOperationSpec.m
@@ -12,9 +12,52 @@
 #define HC_SHORTHAND
 #import <OCHamcrest/OCHamcrest.h>
 
-#import <OCMock/OCMock.h>
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
 
 #import "ParseJsonOperation.h"
+
+
+@interface BlockedParseJsonOperation : ParseJsonOperation
+@end
+
+@implementation BlockedParseJsonOperation
+{
+    BOOL _blocked;
+    NSCondition *_blockLock;
+}
+
+- (instancetype)init {
+    self = [super init];
+
+    _blocked = NO;
+    _blockLock = [[NSCondition alloc] init];
+
+    return self;
+}
+
+- (void)block {
+    [_blockLock lock];
+    _blocked = YES;
+    [_blockLock unlock];
+}
+
+- (void)unblock {
+    [_blockLock lock];
+    _blocked = NO;
+    [_blockLock signal];
+    [_blockLock unlock];
+}
+
+- (void)main {
+    [_blockLock lock];
+    while (_blocked) {
+        [_blockLock wait];
+    }
+    [_blockLock unlock];
+}
+
+@end
 
 
 SpecBegin(ParseJsonOperation)
@@ -32,13 +75,7 @@ describe(@"ParseJsonOperation", ^{
     it(@"is not ready until json url is set", ^{
         ParseJsonOperation *op = [[ParseJsonOperation alloc] init];
 
-        id observer = observer = OCMStrictClassMock([NSObject class]);
-        [observer setExpectationOrderMatters:YES];
-
-        OCMExpect([observer observeValueForKeyPath:@"isReady" ofObject:op change:hasEntry(NSKeyValueChangeNotificationIsPriorKey, @YES) context:NULL]);
-        OCMExpect([observer observeValueForKeyPath:@"jsonUrl" ofObject:op change:hasEntry(NSKeyValueChangeNotificationIsPriorKey, @YES) context:NULL]);
-        OCMExpect([observer observeValueForKeyPath:@"jsonUrl" ofObject:op change:instanceOf([NSDictionary class]) context:NULL]);
-        OCMExpect([observer observeValueForKeyPath:@"isReady" ofObject:op change:instanceOf([NSDictionary class]) context:NULL]);
+        id observer = mock([NSObject class]);
 
         [op addObserver:observer forKeyPath:@"isReady" options:NSKeyValueObservingOptionPrior context:NULL];
         [op addObserver:observer forKeyPath:@"jsonUrl" options:NSKeyValueObservingOptionPrior context:NULL];
@@ -49,7 +86,14 @@ describe(@"ParseJsonOperation", ^{
         op.jsonUrl = [NSURL URLWithString:@"/metadata.json"];
 
         expect(op.isReady).to.equal(YES);
-        OCMVerifyAll(observer);
+
+        [verify(observer) observeValueForKeyPath:@"isReady" ofObject:op change:hasEntry(NSKeyValueChangeNotificationIsPriorKey, @YES) context:NULL];
+        [verify(observer) observeValueForKeyPath:@"jsonUrl" ofObject:op change:hasEntry(NSKeyValueChangeNotificationIsPriorKey, @YES) context:NULL];
+        [verify(observer) observeValueForKeyPath:@"jsonUrl" ofObject:op change:isNot(hasEntry(NSKeyValueChangeNotificationIsPriorKey, @YES)) context:NULL];
+        [verify(observer) observeValueForKeyPath:@"isReady" ofObject:op change:isNot(hasEntry(NSKeyValueChangeNotificationIsPriorKey, @YES)) context:NULL];
+
+        [op removeObserver:observer forKeyPath:@"isReady"];
+        [op removeObserver:observer forKeyPath:@"jsonUrl"];
     });
 
     it(@"is not ready until dependencies are finished", ^{
@@ -65,32 +109,41 @@ describe(@"ParseJsonOperation", ^{
 
         NSPredicate *isFinished = [NSPredicate predicateWithFormat:@"finished = YES"];
         [self expectationForPredicate:isFinished evaluatedWithObject:holdup handler:nil];
-        [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * _Nullable error) {
+        [self waitForExpectationsWithTimeout:1.0 handler:^(NSError * _Nullable error) {
             expect(op.ready).to.equal(YES);
         }];
     });
 
-    it(@"throws an exception when dest dir change is attempted while executing", ^{
-        ParseJsonOperation *op = [[ParseJsonOperation alloc] init];
+    it(@"throws an exception when json url change is attempted while executing", ^{
+        BlockedParseJsonOperation *op = [[BlockedParseJsonOperation alloc] init];
         op.jsonUrl = [NSURL URLWithString:@"/metadata.json"];
 
-        ParseJsonOperation *mockOp = OCMPartialMock(op);
-        OCMStub([mockOp isExecuting]).andReturn(YES);
+        [op block];
+
+        NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+        [queue addOperation:op];
+
+        [self expectationForPredicate:[NSPredicate predicateWithFormat:@"isExecuting == YES"] evaluatedWithObject:op handler:nil];
+        [self waitForExpectationsWithTimeout:1.0 handler:nil];
 
         expect(^{
             op.jsonUrl = [NSURL URLWithString:[NSString stringWithFormat:@"/var/%@", op.jsonUrl.path]];
         }).to.raiseWithReason(@"IllegalStateException", @"cannot change jsonFileUrl after ParseReportMetaDataOperation has started");
-        
+
+        [op unblock];
+
+        [self expectationForPredicate:[NSPredicate predicateWithFormat:@"isFinished == YES"] evaluatedWithObject:op handler:nil];
+        [self waitForExpectationsWithTimeout:1.0 handler:nil];
+
         expect(op.jsonUrl).to.equal([NSURL URLWithString:@"/metadata.json"]);
     });
 
     it(@"parses the json", ^{
         ParseJsonOperation *op = [[ParseJsonOperation alloc] init];
-        op.jsonUrl = [NSURL URLWithString:@"/tmp/metadata.json"];
 
-        NSString *jsonString = @"{\"didItWork\": true, \"number\": 28, \"array\": [1, 2, 3], \"string\": \"ner ner\", \"object\": { \"key\": \"value\" } }";
-        id mockDataClass = OCMClassMock([NSData class]);
-        OCMExpect([mockDataClass dataWithContentsOfURL:op.jsonUrl]).andReturn([jsonString dataUsingEncoding:NSUTF8StringEncoding]);
+        NSBundle *bundle = [NSBundle bundleForClass:[ParseJsonOperationSpec class]];
+        NSURL *jsonUrl = [NSURL fileURLWithPath:[bundle pathForResource:@"ParseJsonOperationSpec" ofType:@"json"]];
+        op.jsonUrl = jsonUrl;
 
         [op start];
 
